22ead4dc1461a37cdb9cb8f9bf8c29854ac9a32b
sora1129 <64789798+sora1129@users.noreply.github.com>
2022-02-18 13:22:14 +0900

    URLに応じて処理を変える→HTMLファイルの作成、読み込み処理

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const http = require('http'); //httpモジュールの読み込み（決まり文句みたいなもの）
-
-const hostname = '127.0.0.1'; // ホスト名を指定
-const port = 3000; // ポートを指定
+	fs = require('fs');
+const settings = require('./settings');
+console.log(settings);
 
 const server = http.createServer((req, res) => { // サーバーを作って引数を渡す
-  res.statusCode = 200; //ステータスコードを設定（200：OK リクエストが正常に処理できた）
-  res.setHeader('Content-Type', 'text/plain'); //コンテンツタイプを持つレスポンスHTTPヘッダーを設定
-  res.end('Hello World'); // リクエストの送信を終了
+	fs.readFile(__dirname + '/public_html/hello.html', 'utf-8', function(err, data) {
+		if (err) {
+			res.statusCode = 404;
+			res.setHeader('Content-Type', 'text/plain');
+			return res.end("not found!");
+		}
+		// 上手くファイルが読み込まれた時の処理
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/html'); 
+		res.end(data);
+	});
 });
 
 // 3000 番ポートでリクエストを待機し、受信したときにログ出力するコールバック関数
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(settings.port, settings.hostname, () => {
+  console.log(`Server running at http://${settings.hostname}:${settings.port}/`);
 });
---
+++ settings.js
@@ -0,0 +1,2 @@
+exports.port = 3000; //ポート番号を指定
+exports.hostname = '127.0.0.1'; // ホスト名を指定
